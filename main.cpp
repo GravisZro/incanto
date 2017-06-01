4b6baa71f2e8ef44b1fb9f2cabf4926dd416396c
Gravis <github@adaptivetime.com>
2017-06-01 12:23:51 -0400

    no idea ;)

--- main.cpp
+++ main.cpp
@@ -24,9 +24,13 @@ int main(int argc, char *argv[])
 
   if(printer != nullptr)
   {
-    printer->local_functions.push_back({"local_example", {{"posix::fd_t", "fd"}, {"long", "demo"}, {"std::string", "lol"}}});
-    printer->remote_functions.push_back({"remote_example", {{"std::string", "lulz"}, {"std::vector<int>", "arr"}}});
-    printer->local_functions.push_back({"local_example2", {{"std::string", "lulz"}, {"std::vector<int>", "arr"}}});
+
+    throw(std::system_error((int)std::errc::invalid_argument, std::generic_category(), "Pointers and references are not allowed with the exception of string literals."));
+    throw(std::system_error((int)std::errc::invalid_argument, std::generic_category(), "Only \"fixed width integer types\", STL containers thereof, strings and posix::fd_t types are allowed."));
+
+    printer->local_functions .push_back({"local_example"  , {{"posix::fd_t", "fd"}, {"long", "demo"}, {"std::string", "lol"}}});
+    printer->remote_functions.push_back({"remote_example" , {{"std::string", "lulz"}, {"std::vector<int>", "arr"}}});
+    printer->local_functions .push_back({"local_example2" , {{"std::string", "lulz"}, {"std::vector<int>", "arr"}}});
     printer->remote_functions.push_back({"remote_example2", {{"long", "demo"}, {"posix::fd_t", "fd"}, {"std::string", "lol"}}});
 
     try
